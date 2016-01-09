6dd33261c18d314c2e2c8b62c13a6353bb801b49
liuqi <1162960595@qq.com>
2016-01-09 13:27:53 +0800

    add

---
+++ src/com/liuqi/main/SystemMain.java
@@ -0,0 +1,16 @@
+package com.liuqi.main;
+
+public class SystemMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("helloword");
+		
+		System.out.println("hello");
+	}
+
+}
